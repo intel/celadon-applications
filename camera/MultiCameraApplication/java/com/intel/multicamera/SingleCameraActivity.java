abec4fde45930ede68211a1f8c4b8c2ad6278557
kbillore <kaushal.billore@intel.com>
2020-06-10 13:38:16 +0530

    Disable camera icon when no camera detected
    Root-Cause : Automation scripts works based on icons
    visibile on screen. When no camera detected app still
    showing icons because of that camera test case was
    passed.
    
    Solution : Disbale camera icons when no camera detected
    
    Signed-off-by: Shiva Kumara  <shiva.kumara.rudrappa@intel.com>
    Signed-off-by: kbillore <kaushal.billore@intel.com>

--- camera/MultiCameraApplication/java/com/intel/multicamera/SingleCameraActivity.java
+++ camera/MultiCameraApplication/java/com/intel/multicamera/SingleCameraActivity.java
@@ -214,8 +214,20 @@ public class SingleCameraActivity extends AppCompatActivity {
         if (numOfCameras == 0) {
             Toast.makeText(SingleCameraActivity.this, "No Camera Found", Toast.LENGTH_LONG).show();
             System.out.println(TAG+" no camera found");
+            mCameraSwitch.setEnabled(false);
+            mCameraPicture.setEnabled(false);
+            mCameraRecord.setEnabled(false);
+            mCameraSplit.setEnabled(false);
+            mSettings.setEnabled(false);
             return;
         }
+
+        mCameraSwitch.setEnabled(true);
+        mCameraPicture.setEnabled(true);
+        mCameraRecord.setEnabled(true);
+        mCameraSplit.setEnabled(true);
+        mSettings.setEnabled(true);
+
         if (numOfCameras == 1) {
             findViewById(R.id.camera_switch).setVisibility(View.GONE);
             findViewById(R.id.camera_split_view).setVisibility(View.GONE);
